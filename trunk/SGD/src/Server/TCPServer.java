ffb96566fde7f0cb38861a850d23a75d05533432
dfduarte@student.dei.uc.pt <dfduarte@student.dei.uc.pt@681abef1-7696-2c74-66b9-2b0365c7c304>
2011-03-25 21:52:45 +0000

    ´.!.

--- trunk/SGD/src/Server/TCPServer.java
+++ trunk/SGD/src/Server/TCPServer.java
@@ -17,13 +17,12 @@ class TCPServer {
     public static void main(String argv[]) throws Exception {
 
         ServerSocket welcomeSocket = new ServerSocket(6789);
-
-        while (true) {
-            Socket connectionSocket = welcomeSocket.accept();
-
-            InputStream in = connectionSocket.getInputStream();
+        Socket connectionSocket;
+        connectionSocket= welcomeSocket.accept();
+        InputStream in = connectionSocket.getInputStream();
             DataInputStream dis = new DataInputStream(in);
-
+            int v=0;
+        while (true) {
 
             int len = dis.readInt();
 
@@ -31,27 +30,29 @@ class TCPServer {
             if (len > 0) {
                 dis.readFully(data);
             }
-            System.out.println(data);
-            System.out.println(data.length);
+            //System.out.println(data);
+            //System.out.println(data.length);
             toObject(data);
-
+            v++;
+            System.out.println(v);
         }
+
     }
 
     public static Object toObject(byte[] bytes) {
 
 
-        JSONObject object = null;
+        Object object = null;
    
         try {
 
-            object = (JSONObject) new java.io.ObjectInputStream(new java.io.ByteArrayInputStream(bytes)).readObject();
+            object =  new java.io.ObjectInputStream(new java.io.ByteArrayInputStream(bytes)).readObject();
         } catch (java.io.IOException ioe) {
             System.out.println("aaa");
              java.util.logging.Logger.global.log(java.util.logging.Level.SEVERE,ioe.getMessage());
         } catch (java.lang.ClassNotFoundException cnfe) {
         }
-        System.out.println(object);
+        //System.out.println(object);
         return object;
     }
 }
