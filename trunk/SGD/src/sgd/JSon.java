3d99afd95a5c84eb3d862fb97be54e352410c975
dfduarte@student.dei.uc.pt <dfduarte@student.dei.uc.pt@681abef1-7696-2c74-66b9-2b0365c7c304>
2011-03-29 16:16:51 +0000

    ´.!.

--- trunk/SGD/src/sgd/JSon.java
+++ trunk/SGD/src/sgd/JSon.java
@@ -24,7 +24,7 @@ import java.io.Serializable;
 import java.net.Socket;
 import org.json.JSONObject;
 
-public class JSon{
+public class JSon {
 
     public static void main(String[] args) throws Exception {
         Generator gen = new Generator();
@@ -37,50 +37,34 @@ public class JSon{
 
         for (i = 0; i < 100000; i++) {
 
-        JSONObject json = new JSONObject();
-        json.put("caller_id", dados[i].getCaller_id());
-        json.put("duration", dados[i].getDuration());
-        json.put("billsec", dados[i].getBillsec());
-        json.put("progresssec", dados[i].getProgressec());
-        json.put("progress_mediasec", dados[i].getProgress_mediasec());
-        json.put("flow_billsec", dados[i].getBillsec());
-        json.put("mduration", dados[i].getMduration());
-        json.put("billmsec", dados[i].getBillmsec());
-        json.put("progressmsec", dados[i].getProgressmsec());
-        json.put("progress_mediamsec", dados[i].getProgress_mediamsec());
-        json.put("flow_billmsec", dados[i].getFlow_billmsec());
-        json.put("uduration", dados[i].getUduration());
+            JSONObject json = new JSONObject();
+            json.put("caller_id", dados[i].getCaller_id());
+            json.put("duration", dados[i].getDuration());
+            json.put("billsec", dados[i].getBillsec());
+            json.put("progresssec", dados[i].getProgressec());
+            json.put("progress_mediasec", dados[i].getProgress_mediasec());
+            json.put("flow_billsec", dados[i].getBillsec());
+            json.put("mduration", dados[i].getMduration());
+            json.put("billmsec", dados[i].getBillmsec());
+            json.put("progressmsec", dados[i].getProgressmsec());
+            json.put("progress_mediamsec", dados[i].getProgress_mediamsec());
+            json.put("flow_billmsec", dados[i].getFlow_billmsec());
+            json.put("uduration", dados[i].getUduration());
 
-        //System.out.println(":" + json);
-        example = json.toString();
-        byte[] teste = convert.toBytes(example);
-       // System.out.println(teste);
-       
+            //System.out.println(":" + json);
+            example = json.toString();
+            byte[] teste = convert.toBytes(example);
+            // System.out.println(teste);
 
-        //toObject(teste);
-        sendBytes(teste, 0, teste.length, clientSocket);
 
-            if(i==99000)
-                i=0;
+            //toObject(teste);
+            sendBytes(teste, 0, teste.length, clientSocket);
+
+            if (i == 99000) {
+                i = 0;
+            }
 
         }
-    }
-
-    public static byte[] toBytes(Object object) throws IOException {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutput out = new ObjectOutputStream(bos);
-        out.writeObject(object);
-        return bos.toByteArray();
-    }
-
-    public static Object toObject(byte[] bytes) throws IOException, ClassNotFoundException {
-
-
-       ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-        ObjectInput in = new ObjectInputStream(bis);
-        Object o = in.readObject();
-        //System.out.println(0);
-        return o;
     }
 
     public static void sendBytes(byte[] myByteArray, int start, int len, Socket clientSocket) throws IOException {
