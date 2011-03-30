59cf3691e1fdd75e9a11149aa51ec2fd299d8978
dfduarte@student.dei.uc.pt <dfduarte@student.dei.uc.pt@681abef1-7696-2c74-66b9-2b0365c7c304>
2011-03-30 03:34:11 +0000

    ´.!.

--- trunk/SGD/src/Server/UDPServer.java
+++ trunk/SGD/src/Server/UDPServer.java
@@ -10,28 +10,59 @@ package Server;
  */
 import java.io.*;
 import java.net.*;
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Node;
 import sgd.BytetoObject;
+import sgd.ConverttoXML;
+
+import Client.Generator;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.w3c.dom.*;
+
+import javax.xml.parsers.*;
+
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
 
 class UDPServer {
 
     public static void main(String args[]) throws Exception {
         DatagramSocket serverSocket = new DatagramSocket(9876);
-        int v=0;
+        ConverttoXML t = new ConverttoXML();
+        int v = 0;
         byte[] receiveData = new byte[1024];
         BytetoObject convert = new BytetoObject();
         Object obj = null;
-        byte[] sendData = new byte[1024];
+
         while (true) {
 
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String sentence = new String(receivePacket.getData());
-            
-           
+
+
             obj = convert.toObject(receivePacket.getData());
             v++;
-            System.out.println(v);
-            System.out.println(obj);
+
+
+
+            t.conv(obj);
+
+            //System.out.println("-------------");
         }
     }
 }
